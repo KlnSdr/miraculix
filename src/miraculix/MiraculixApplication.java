a777217eeb690bf13a53562cd1690375489424ab
KlnSdr <monsieurk1209@gmail.com>
2025-02-28 11:10:27 +0100

    setup hades

---
+++ src/miraculix/MiraculixApplication.java
@@ -0,0 +1,9 @@
+package miraculix;
+
+import hades.Hades;
+
+public class MiraculixApplication extends Hades {
+    public static void main(String[] args) {
+        new MiraculixApplication().startApplication(MiraculixApplication.class);
+    }
+}
